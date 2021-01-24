071f7e7da0f42b5f3bc7bf69ec5a934b35afd29a
liaochong <liaochong@guahao.com>
2021-01-24 15:17:39 +0800

    配置清理

---
+++ alkaid-provider/src/main/java/com/github/liaochong/alkaid/provider/Provider.java
@@ -0,0 +1,18 @@
+package com.github.liaochong.alkaid.provider;
+
+/**
+ * @author liaochong
+ * @version 1.0
+ */
+public class Provider {
+
+    private Class<?> providerClass;
+
+    public Class<?> getProviderClass() {
+        return providerClass;
+    }
+
+    public void setProviderClass(Class<?> providerClass) {
+        this.providerClass = providerClass;
+    }
+}
